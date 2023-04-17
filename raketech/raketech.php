b6053524abe85d8c2b2b32e48d6dcaac3e0cd173
Henrique Ferreira <henrique.f.dutra@gmail.com>
2023-04-17 15:52:08 +0100

    Creating the plugin

---
+++ raketech/raketech.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Plugin Raketech
+ * Plugin URI: https://github.com/hfdutra/raketech
+ * Description: Plugin task for the role of WordPress Developer at Raketech.
+ * Version: 1.0.0
+ * Author: Henrique Ferreira
+ * Author URI: https://github.com/hfdutra/
+ * License: GPL2
+ */
+
+?>
